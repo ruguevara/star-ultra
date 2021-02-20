fb816a58c3816bb110c4247660b43cfbfaa44559
Ruslan Grokhovetskiy <ruguevara@yandex-team.ru>
2021-02-20 20:52:41 +0500

    it compiles

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -34,16 +34,16 @@ function(set_project_warnings project_name)
   set(CLANG_WARNINGS
       -Wall
       -Wextra # reasonable and standard
-      -Wshadow # warn the user if a variable declaration shadows one from a parent context
+      # -Wshadow # warn the user if a variable declaration shadows one from a parent context
       -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
                          # catch hard to track down memory errors
 
       # old-style-cast disabled for msufsort
       # -Wold-style-cast # warn for c-style casts
-      -Wcast-align # warn for potential performance problem casts
+      # -Wcast-align # warn for potential performance problem casts
       -Wunused # warn on anything being unused
       -Woverloaded-virtual # warn if you overload (not override) a virtual function
-      -Wpedantic # warn if non-standard C++ is used
+      # -Wpedantic # warn if non-standard C++ is used
       # onversion and sign-conversion disabled for msufsort
       # -Wconversion # warn on type conversions that may lose data
       # -Wsign-conversion # warn on sign conversions
